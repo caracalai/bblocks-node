d8dc622dcc09d10f9dfec187fd26dde940b1b953
Tikhon Zaikin <th@thevhs.club>
2021-08-16 11:56:32 +0200

    make cross-language summator work

--- tests/summator/graph.js
+++ tests/summator/graph.js
@@ -1,36 +1,36 @@
 const graph = {
   'server-fabric': 'nodejs-service',
   nodes: {
-    'GeneratorA_01e74be0-2f4f-438d-bca1-b9fd5db5b6cb': {
+    'GeneratorA_d79073f9-6f94-49a7-a4c5-e2783647928f': {
       type: {
         name: 'GeneratorA',
         fabric: 'nodejs-service',
       },
     },
-    'GeneratorB_4adbf5fb-8e66-4aa6-950b-c981a6de4af5': {
+    'GeneratorB_6f865f02-0d91-47b0-aa40-a65867bd5afd': {
       type: {
         name: 'GeneratorB',
         fabric: 'nodejs-service',
       },
     },
-    'Summator_16c68928-e1d9-4da6-b9a7-06650cfa6d8b': {
+    'Summator_7c444c17-9311-4b8a-b34b-14534a2f22a5': {
       type: {
         name: 'Summator',
-        fabric: 'nodejs-service',
+        fabric: 'python-service',
       },
     },
   },
   edges: [
     {
-      source: 'GeneratorA_01e74be0-2f4f-438d-bca1-b9fd5db5b6cb',
+      source: 'GeneratorA_d79073f9-6f94-49a7-a4c5-e2783647928f',
       event: 'value',
-      destination: 'Summator_16c68928-e1d9-4da6-b9a7-06650cfa6d8b',
+      destination: 'Summator_7c444c17-9311-4b8a-b34b-14534a2f22a5',
       handler: 'first',
     },
     {
-      source: 'GeneratorB_4adbf5fb-8e66-4aa6-950b-c981a6de4af5',
+      source: 'GeneratorB_6f865f02-0d91-47b0-aa40-a65867bd5afd',
       event: 'value',
-      destination: 'Summator_16c68928-e1d9-4da6-b9a7-06650cfa6d8b',
+      destination: 'Summator_7c444c17-9311-4b8a-b34b-14534a2f22a5',
       handler: 'second',
     },
   ],
